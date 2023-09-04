e654091055674898676171b1bd677f63b2c86f56
Goh-Uday <uday@gohoardings.com>
2023-09-04 15:54:18 +0530

    final

--- pages/api/excelPPT/index.js
+++
@@ -1,8 +0,0 @@
-import nc from 'next-connect'
-import error from '@/server/utils/error'
-import { excelPPT } from '@/server/controller/cartItems'
-
-
-const handler = nc({error})
-   handler.post(excelPPT)
-export default handler
--- pages/api/testmonials/index.js
+++
@@ -1,7 +0,0 @@
-import nc from 'next-connect'
-import error from '@/server/utils/error'
-import { goh_testimonials } from '@/server/controller/staticPageControl'
-
-const handler = nc(error)
-    handler.get(goh_testimonials)
-export default handler
